ed66a275cf795351ede2897baa9e1dfddc2e83df
paraskaretiya <paraskaretiya@gmail.com>
2020-01-14 19:11:42 +0530

    Updates

---
+++ precache-manifest.c351f7b6cadebc5783fa824d13589d30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b773aacec8493b6ac315a4f0c1ce9c",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "21aa0b9a108aa5a240d1",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ff59d134bd38461e3b5a",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "21aa0b9a108aa5a240d1",
+    "url": "/contactmanager/static/js/2.851a370c.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.851a370c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff59d134bd38461e3b5a",
+    "url": "/contactmanager/static/js/main.17ff65c3.chunk.js"
+  },
+  {
+    "revision": "c9b248b7ef52cebe72fa",
+    "url": "/contactmanager/static/js/runtime-main.bc9037e5.js"
+  }
+]);
